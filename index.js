7545653dfeb9012c25fc715804bcbbde4d453be6
kalkdiansebsibe <sebsibekalkidan61@gmail.com>
2025-05-30 22:47:00 +0300

    pseudocode finished

---
+++ index.js
@@ -0,0 +1,28 @@
+//get user choice
+   //asek th eplayer to choose form th three option
+   //assign the user choice to vairable
+//get computer choice
+   //coshhe random number b/n 1 to 3
+   //assing random number is 1 2 3 to rock  paper or scissor 
+   //assign the computer choice to variable
+//compare the two 
+//if the user choice and the computer choice is equal
+//then its adraw
+//if the user choice is scissors and the computer choice is rock 
+//then the computer wins
+//if the user choice is scissors and the computer choice is paper 
+//then the user win
+//if the user choice is rock and the computer choice is scissors 
+//then the user win
+//if the user choice is rock and the computer choice is paper
+//then the computer win
+//if the user choice is paper and the computer choice is scissors 
+//then the computer win
+//if the user choice is paper and the computer choice is rock
+//then the user win
+//play again message pop up
+
+
+
+
+
